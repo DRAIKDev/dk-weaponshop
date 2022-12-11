f1d723de3a5f668dfe894e1d9596c3ff24bb366c
DRAIK <draikdev@gmail.com>
2022-12-11 14:50:42 +0100

    arreglos varios

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Draik <! Draik#3986>'
+description 'ESX WEAPON SHOP OPTIMICED'
+version '1.0'
+
+shared_script "sh-config.lua"
+server_scripts {
+	'@es_extended/locale.lua','locales/de.lua','locales/br.lua','locales/en.lua','locales/fi.lua','locales/fr.lua','locales/es.lua','locales/pl.lua','locales/sv.lua','s-main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua','locales/de.lua','locales/br.lua','locales/en.lua','locales/fi.lua','locales/fr.lua','locales/es.lua','locales/pl.lua','locales/sv.lua','c-main.lua','c-submain.lua'
+}
